f802083ae13a54db030cb6027b5645813fea7180
Hamsavardhini Karthikayan <hamsavardhinikarthikayan@gmail.com>
2026-06-28 14:08:25 +0530

    Add files via upload

---
+++ DeepSkilling/Week2/Spring Core_Maven/Exercise 1: Configuring a Basic Spring Application/BookRepository.java
@@ -0,0 +1,12 @@
+package com.library.repository;
+
+public class BookRepository {
+
+    public BookRepository() {
+        System.out.println("BookRepository object created");
+    }
+
+    public void display() {
+        System.out.println("Book Repository Working");
+    }
+}
---
+++ DeepSkilling/Week2/Spring Core_Maven/Exercise 1: Configuring a Basic Spring Application/BookService.java
@@ -0,0 +1,12 @@
+package com.library.service;
+
+public class BookService {
+
+    public BookService() {
+        System.out.println("BookService object created");
+    }
+
+    public void service() {
+        System.out.println("Book Service Working");
+    }
+}
