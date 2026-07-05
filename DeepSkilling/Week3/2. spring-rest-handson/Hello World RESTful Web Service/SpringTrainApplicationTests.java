e90e53bd8c7953f73799770a6f7f354ed27fdf0f
Hamsavardhini Karthikayan <hamsavardhinikarthikayan@gmail.com>
2026-07-05 23:39:52 +0530

    Add files via upload

---
+++ DeepSkilling/Week3/2. spring-rest-handson/Hello World RESTful Web Service/SpringTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.spring_train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
