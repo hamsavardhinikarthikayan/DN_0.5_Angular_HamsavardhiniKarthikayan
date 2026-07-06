e2ebb1e0c39d62a50c0f0100ce849abe22ccbef7
Hamsavardhini Karthikayan <hamsavardhinikarthikayan@gmail.com>
2026-07-06 14:43:23 +0530

    Add files via upload

---
+++ DeepSkilling/Week3/2. spring-rest-handson/REST - Get country based on country code/CountryController.java
@@ -0,0 +1,59 @@
+package com.cognizant.spring_train.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.spring_train.Country;
+import com.cognizant.spring_train.service.CountryService;
+
+@RestController
+public class CountryController {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(CountryController.class);
+
+    @Autowired
+    private CountryService countryService;
+
+    // Hands-on 1.2
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+
+        LOGGER.info("START");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country =
+                context.getBean("country1", Country.class);
+
+        LOGGER.info("Country : {}", country);
+
+        LOGGER.info("END");
+
+        return country;
+    }
+
+    // Hands-on 1.3
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+
+        LOGGER.info("START");
+
+        Country country = countryService.getCountry(code);
+
+        LOGGER.info("Country : {}", country);
+
+        LOGGER.info("END");
+
+        return country;
+    }
+
+}
---
+++ DeepSkilling/Week3/2. spring-rest-handson/REST - Get country based on country code/SpringTrainApplication.java
@@ -0,0 +1,15 @@
+package com.cognizant.spring_train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrainApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringTrainApplication.class, args);
+
+    }
+
+}
