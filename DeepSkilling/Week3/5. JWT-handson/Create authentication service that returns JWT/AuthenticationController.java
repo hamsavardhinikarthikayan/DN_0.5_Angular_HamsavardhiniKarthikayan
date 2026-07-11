ccf5a114ca6c045cb019cc3956557abb23365295
Hamsavardhini Karthikayan <hamsavardhinikarthikayan@gmail.com>
2026-07-11 22:11:35 +0530

    Create AuthenticationController.java

---
+++ DeepSkilling/Week3/5. JWT-handson/Create authentication service that returns JWT/AuthenticationController.java
@@ -0,0 +1,10 @@
+package com.cognizant.spring_train.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class AuthenticationController {
+    @GetMapping("/authenticate")
+    public String authenticate() {
+        return "Authentication Successful";
+    }
+}
