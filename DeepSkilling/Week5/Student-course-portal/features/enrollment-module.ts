b58b2230de1d7fb8222b405e7482e6b9534c095c
Hamsavardhini Karthikayan <hamsavardhinikarthikayan@gmail.com>
2026-07-20 00:20:50 +0530

    Add files via upload

---
+++ DeepSkilling/Week5/Student-course-portal/features/enrollment-module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+
+import { EnrollmentRoutingModule } from './enrollment-routing-module';
+
+@NgModule({
+
+  imports: [
+
+    EnrollmentRoutingModule
+
+  ]
+
+})
+
+export class EnrollmentModule { }
